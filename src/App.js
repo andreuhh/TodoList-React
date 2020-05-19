708dc3362db0f8d73dc88963648a0306018d35e3
Andrea Guffi <andreaguffi@MBP-di-Andrea-2.homenet.telecomitalia.it>
2020-05-19 21:04:07 +0200

    input ok

--- src/App.js
+++ src/App.js
@@ -9,33 +9,50 @@ import TodoList from './components/TodoList';
 // showing vs-code github setup
 class App extends Component {
   state = {
-    items: [
-      { id: 1, title: 'wake up' },
-      { id: 2, title: 'breakfast' },
-    ],
+    items: [],
     id: uuidv4(),
     item: '',
     editItem: false,
   };
 
   handleChange = (e) => {
-    console.log('handle change');
+    this.setState({
+      item: e.target.value,
+    });
   };
+
   handleSubmit = (e) => {
-    console.log('handle Submit');
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState(
+      {
+        items: updatedItems,
+        item: '',
+        id: uuidv4(),
+        editItem: false,
+      },
+      () => console.log(this.state)
+    );
   };
+
   clearList = (e) => {
     console.log('clear list');
   };
+
   handleDelete = (id) => {
     console.log(`handle delete ${id}`);
   };
+
   handleEdit = (id) => {
     console.log(`handle edit ${id}`);
   };
 
   render() {
-    console.log(this.state);
     return (
       <div className="container">
         <div className="row">
